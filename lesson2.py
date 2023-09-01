8a4ce54d73d81093644ca0b95d2099e17d23d83a
wisp_mac <mail.for.wisp@gmail.com>
2023-09-01 19:48:44 +0200

    added first examples

--- lesson2.py
+++ lesson2.py
@@ -1,17 +1,32 @@
-# умови
-# v1
-n1 = 10 + 20 * 2 # оператор привласнення "=" відпрацьовує останнім
-n2 = 20
+# # умови
+# # v1
+# n1 = 10 + 20 * 2 # оператор привласнення "=" відпрацьовує останнім
+# n2 = 20
+#
+# # v2
+# n1, n2 = 10, 20 #множинне привласнення
+# n1 == n2 #перше дірівнює другому
+# n1 != n2 #перше НЕ дорівнює другому
+#
+# # v3
+#
+# is_valid = True
+# print(is_valid)
+# print(not is_valid)
+#
+# print('hello' in 'hello word')
 
-# v2
-n1, n2 = 10, 20 #множинне привласнення
-n1 == n2 #перше дірівнює другому
-n1 != n2 #перше НЕ дорівнює другому
+# # v1
+# hours = int(input('Enter hours: '))
+# if hours >= 12 and >= 1 and <= 24:
+#     print('PM')
+# else:
+#     print('AM')
 
-# v3
-
-is_valid = True
-print(is_valid)
-print(not is_valid)
-
-print('hello' in 'hello word')
+hours = int(input('Enter hours: '))
+if hours >= 12 and hours <= 24:
+    print('PM')
+elif hours >= 1 and hours <= 12:
+    print('AM')
+else:
+    print('Incorrect hours')
