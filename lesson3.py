6dd64ecc16be1e9285cd30652ad455866bb9f0ee
wisp_mac <mail.for.wisp@gmail.com>
2023-09-07 10:14:06 +0300

    Merge conflict example

---
+++ lesson3.py
@@ -0,0 +1,18 @@
+# # n1, n2 = 10, 0
+# # print(n1 / n2)
+#
+# num = float(input('Enter your number: '))
+# print(num)
+#
+# try:
+#     num1 = int(input('Enter first number: '))
+#     num2= int(input('Enter second number: '))
+#
+#     result  = num1/num2
+# except ZeroDivisionError as error:
+#     print(f"ZerroDivigionError occured: {error}")
+# except ValueError as error:
+#     print('Enter only integer number please!')
+#     print(f'ValueError')
+
+print('Merge conflict example')
