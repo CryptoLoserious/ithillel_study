197ed600e2afbfe08013bb015473e65181d38383
wisp_mac <mail.for.wisp@gmail.com>
2023-09-07 10:17:04 +0300

    Merge conflict example

--- lesson2.py
+++ lesson2.py
@@ -1,48 +1,50 @@
-# n1 = int(input('Input first number: '))
-# n2 = int(input('Input second number: '))
-# n3 = int(input('Input third number: '))
-#
-# value = input('Select choice: "MIN", "MAX", "MID_SUM": ')
-#
-# #MIN
-# if value == "MIN":
-#     if n1 < n2 and n1 < n3:
-#         print('MIN = ', n1)
-#     elif n2 < n1 and n2 < n3:
-#         print('MIN = ', n2)
-#     elif n3 < n1 and n3 < n2:
-#         print('MIN = ', n3)
+# # n1 = int(input('Input first number: '))
+# # n2 = int(input('Input second number: '))
+# # n3 = int(input('Input third number: '))
+# #
+# # value = input('Select choice: "MIN", "MAX", "MID_SUM": ')
+# #
+# # #MIN
+# # if value == "MIN":
+# #     if n1 < n2 and n1 < n3:
+# #         print('MIN = ', n1)
+# #     elif n2 < n1 and n2 < n3:
+# #         print('MIN = ', n2)
+# #     elif n3 < n1 and n3 < n2:
+# #         print('MIN = ', n3)
+# # # elif value != "MAX" and value != "MIN" and value != "MID_SUM":
+# # #     print('Input integer number!')
+# #
+# # #MAX
+# #     if value == "MAX":
+# #         if n1 > n2 and n1 > n3:
+# #             print('MAX = ', n1)
+# #         elif n2 > n1 and n2 > n3:
+# #             print('MAX = ',n2)
+# #         elif n3 > n1 and n3 > n2:
+# #             print('MAX = ',n3)
+# # # elif value != "MAX" and value != "MIN" and value != "MID_SUM":
+# # #     print('Input integer number!')
+# #
+# # #MID_SUM
+# # if value == "MID_SUM":
+# #     print('MID_SUM = ', ((n1 + n2 + n3)/3))
 # # elif value != "MAX" and value != "MIN" and value != "MID_SUM":
-# #     print('Input integer number!')
+# #     print('Input correct amounts!')
 #
-# #MAX
-#     if value == "MAX":
-#         if n1 > n2 and n1 > n3:
-#             print('MAX = ', n1)
-#         elif n2 > n1 and n2 > n3:
-#             print('MAX = ',n2)
-#         elif n3 > n1 and n3 > n2:
-#             print('MAX = ',n3)
-# # elif value != "MAX" and value != "MIN" and value != "MID_SUM":
-# #     print('Input integer number!')
+# d1 = int(input('Input your digit meters: '))
 #
-# #MID_SUM
-# if value == "MID_SUM":
-#     print('MID_SUM = ', ((n1 + n2 + n3)/3))
-# elif value != "MAX" and value != "MIN" and value != "MID_SUM":
+# choice = input('Select your choice: "meter", "inch", "yard", "mile": ')
+#
+# if choice == "meter" or choice == "METER":
+#     print(d1, 'meter(s)')
+# elif choice == "inch" or choice == "INCH":
+#     print(d1, '.m =', d1*39.3701, 'inch(s)')
+# elif choice == "yard" or choice == "YARD":
+#     print(d1, '.m =', d1*1.0936, 'yard(s)')
+# elif choice == "mile" or choice == "MILE":
+#     print(d1, '.m =', d1*0.00062137, 'mile(s)')
+# else:
 #     print('Input correct amounts!')
 
-d1 = int(input('Input your digit meters: '))
-
-choice = input('Select your choice: "meter", "inch", "yard", "mile": ')
-
-if choice == "meter" or choice == "METER":
-    print(d1, 'meter(s)')
-elif choice == "inch" or choice == "INCH":
-    print(d1, '.m =', d1*39.3701, 'inch(s)')
-elif choice == "yard" or choice == "YARD":
-    print(d1, '.m =', d1*1.0936, 'yard(s)')
-elif choice == "mile" or choice == "MILE":
-    print(d1, '.m =', d1*0.00062137, 'mile(s)')
-else:
-    print('Input correct amounts!')
+print('Merge conflict example!')
